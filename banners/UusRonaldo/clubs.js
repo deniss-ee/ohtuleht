97e49041c413800dd1f74833c8b88ececa325090
Deniss Ivanjuk <31806621+deniss-ee@users.noreply.github.com>
2026-05-27 15:32:45 +0300

    Add UusRonaldo banners and assets
    Add a new banners/UusRonaldo package with HTML, CSS, JS, images, SVGs and logos (including a 1250x400 variant, club data, map and ZIP). Move/rename bg.jpg and .DS_Store entries accordingly, and remove outdated banner templates for UusRonaldo. Also bump ad templates/v3_corner.html title to v3.1.1 and update some repository .DS_Store files.

---
+++ banners/UusRonaldo/clubs.js
@@ -0,0 +1,87 @@
+var CLUBS = [
+  {
+    id: "paernu-jk-vaprus",
+    pin: {
+      left: 168,
+      top: 189,
+    },
+    name: "JK Vaprus",
+    city: "Pärnu",
+    logo: "vaprus.png",
+    overlay: {
+      fields: [
+        { label: "Peatreener", value: "Igor Prins", icon: "person.svg" },
+        { label: "E-post", value: "info@vaprus.ee", icon: "mail.svg" },
+      ],
+      url: "https://vaprus.ee/",
+    },
+  },
+  {
+    id: "paernu-jk-poseidon",
+    pin: {
+      left: 184,
+      top: 205,
+    },
+    name: "JK Poseidon",
+    city: "Pärnu",
+    logo: "poseidon.png",
+    overlay: {
+      fields: [
+        { label: "Noortetöö koordinaator", value: "Hendrik Toots", icon: "person.svg" },
+        { label: "E-post", value: "hendrik@jkposeidon.ee", icon: "mail.svg" },
+      ],
+      url: "https://jkposeidon.ee/",
+    },
+  },
+  {
+    id: "rakvere-jk-tarvas",
+    pin: {
+      left: 330,
+      top: 45,
+    },
+    name: "JK Tarvas",
+    city: "Rakvere",
+    logo: "tarvas.png",
+    overlay: {
+      fields: [
+        { label: "peatreener", value: "Reijo Kuusik", icon: "person.svg" },
+        { label: "E-post", value: "noored@jktarvas.ee", icon: "mail.svg" },
+      ],
+      url: "https://jktarvas.ee/",
+    },
+  },
+  {
+    id: "harju-jalgpallikool",
+    pin: {
+      left: 135,
+      top: 45,
+    },
+    name: "Harju Jalgpallikool",
+    city: "Laagri",
+    logo: "harju.png",
+    overlay: {
+      fields: [
+        { label: "peatreener", value: "Lauri Nuuma", icon: "person.svg" },
+        { label: "E-post", value: "info@harjujk.ee", icon: "mail.svg" },
+      ],
+      url: "https://harjujk.ee/",
+    },
+  },
+  {
+    id: "fc-helios-tartu",
+    pin: {
+      left: 355,
+      top: 205,
+    },
+    name: "FC Helios",
+    city: "Tartu",
+    logo: "helios.png",
+    overlay: {
+      fields: [
+        { label: "Tegevjuht", value: "Urmo Viickberg", icon: "person.svg" },
+        { label: "E-post", value: "urmo@fchelios.ee", icon: "mail.svg" },
+      ],
+      url: "https://www.fchelios.ee/",
+    },
+  },
+];
